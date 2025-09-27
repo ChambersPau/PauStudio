b2c7edbd375de662a59eb03ad45a65ab1b8c372a
David07994415 <david.game.power@gmail.com>
2025-09-27 21:18:04 +0800

    Update:Adjust Home Page Card Function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -76,7 +76,7 @@ onMounted(() => {
 
 
     // setTimeout(() => {
-    //     topProds.forEach(item => {
+    //     topProds.value.forEach(item => {
     //         const img = new Image();
     //         img.onload = () => {};
     //         img.onerror = () => {};
@@ -121,7 +121,7 @@ onMounted(() => {
         <section class="cards-section d-flex flex-column justify-content-center gap-0 my-8 fade-in-effect "
             id="cards-section">
             <div class="cards-category d-flex justify-content-center align-items-center gap-2 mb-3">
-                <h3 @click="ShowTopProds" class="fs-1 active">精選作品</h3>
+                <h3 class="fs-1 active">精選作品</h3>
                 <!-- id="title" -->
                 <span class='fs-1'>|</span>
                 <RouterLink to="/category">
@@ -129,29 +129,26 @@ onMounted(() => {
                 </RouterLink>
             </div>
             <div class="cards-wrapper">
-                <button @click="ClickLeftBtn" v-show="isShowLeftBtn" class="carousel-btn left">&#10094;</button>
+                <button @click="ClickLeftBtn" class="carousel-btn left">&#10094;</button>
                 <div v-if="isShowTopProds" ref="tracker" class="cards-tracker">
-                    <div v-for="(topProd, index) in topProds" :key="topProd.videoId" class="card-item">
+                    <div v-for="topProd in topProds" :key="topProd.videoId" class="card-item">
                         <RouterLink
                             :to="{ path: '/product', query: { videoId: topProd.videoId, category: topProd.type } }">
+
+                            <!-- :class="['card-image',
+                                {
+                                    'custom-position-right': topProd.position === 'right',
+                                    'custom-position-left': topProd.position === 'left'
+                                } ]" -->
+
                             <img :class="['card-image',
                                 {
                                     'custom-position-right': topProd.position === 'right',
                                     'custom-position-left': topProd.position === 'left'
-                                }
-                            ]" :src="`https://i.ytimg.com/vi/${topProd.videoId}/maxresdefault.jpg`" alt="Image"
-                                :key="`${topProd.videoId}-${index}`">
+                                }]" :src="`https://i.ytimg.com/vi/${topProd.videoId}/maxresdefault.jpg`" alt="Image">
                         </RouterLink>
                     </div>
                 </div>
-                <!-- <div v-if="!isShowTopProds" ref="tracker" class="cards-tracker">
-                    <div v-for="(category,index) in categories" :key="category.type" class="card-item">
-                        <RouterLink :to="{ path: '/category', query: { type: category.type } }">
-                            <CategoryImgCard :name-ch="category.nameCh" :name-en="category.nameEn"
-                            :type="category.type"/>
-                        </RouterLink>
-                    </div>
-                </div> -->
                 <button @click="ClickRightBtn" v-show="isShowRightBtn" class="carousel-btn right">&#10095;</button>
             </div>
         </section>
